63e90431039de784f91d60aa163856976a24470e
denis <92470430+Mousport@users.noreply.github.com>
2023-09-10 22:25:42 +0300

    test_lesson_1_and_new_test_in_collection

--- tests/first_test.py
+++ tests/first_test.py
@@ -9,5 +9,13 @@ class TestCalc:
         assert self.calc.multiply(self, 2, 2) == 4
 
     def test_multyply_calculate_faled(self):
-        assert self.calc.multiply(self, 2, 2) == 5
+        assert self.calc.multiply(self, 6, 2) == 6
 
+    def test_division_calculate_correctry(self):
+        assert self.calc.division(self, 4, 2) == 2
+
+    def test_subtraction_calculate_correctre(self):
+        assert self.calc.subtraction(self, 8, 6) == 2
+
+    def test_adding_calculate_correctry(self):
+        assert self.calc.adding(self, 4, 8) == 12
